2023652c268af65ccc502f62254d6dedaba26339
RasyidAbdulRauf <rasyidabdulrauf05@gmail.com>
2024-03-26 23:12:06 +0700

    update

--- src/register.php
+++ src/register.php
@@ -1,44 +1,57 @@
 <?php
-    // Connect Database
-    include "service/database.php";
+// Connect Database
+include "service/database.php";
+session_start();
 
-    if(isset($_POST['register'])) {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
 
-        $sql = "INSERT INTO users (username, password) VALUES
+$register_message = "";
+
+if (isset($_SESSION["is_login"])) {
+    header("location: dashboard.php");
+}
+
+if (isset($_POST['register'])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql = "INSERT INTO users (username, password) VALUES
         ('$username', '$password')";
 
-        if ($db->query($sql)) {
-            echo "DATA MASUK";
-        }else {
-            echo "DATA GAGAL MASUK";
-        }
+    if ($db->query($sql)) {
+        $register_message = "Daftar akun berhasil, silahkan login";
+    } else {
+        $register_message = "Daftar akun gagal, silahkan coba lagi";
     }
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
     <!-- Header -->
     <?php include "layout/header.html" ?>
 
+    <h3>DAFTAR AKUN</h3>
+    <i><?= $register_message ?></i>
+    
     <form action="register.php" method="POST">
-        <h3>DAFTAR AKUN</h3>
         <input type="text" placeholder="username" name="username">
         <input type="password" placeholder="password" name="password">
         <button type="submit" name="register">daftar sekarang</button>
     </form>
-
+    
     <!-- Footer -->
     <?php include "layout/footer.html" ?>
-    
+
 </body>
+
 </html>
